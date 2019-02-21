c6669504f99a779f185c571fa95a1d8dcdadfba1
Paul Karikari <paulkarikari1@gmail.com>
2019-02-21 21:31:11 +0100

    change lowerbound to 0

--- karate_shop/karate_shop.go
+++ karate_shop/karate_shop.go
@@ -2,8 +2,34 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
-	fmt.Println(chop(5, []int{9, 6, 3, 5, 7, 1, 2, 4, 10}))
+	fmt.Println(chop(9, []int{9, 6, 3, 5, 7, 1, 2, 4, 10}))
+}
+
+func chop(target int, collection []int) int {
+	sort.Ints(collection)
+	lowerbound := 0
+	upperbound := len(collection)
+
+	for {
+		if upperbound < lowerbound {
+			return -1
+		}
+		midValue := lowerbound + ((upperbound - lowerbound) / 2)
+
+		if collection[midValue] < target {
+			lowerbound = midValue + 1
+		}
+
+		if collection[midValue] > target {
+			upperbound = midValue - 1
+		}
+
+		if collection[midValue] == target {
+			return midValue
+		}
+	}
 }
